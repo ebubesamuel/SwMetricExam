0291f339dfbe99722afbc13b39ab8e7ec579fd62
ebubesamuel <ebubesamuel2003@gmail.com>
2023-01-04 11:39:02 +0200

    first commit

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
-"""This program is to determine the math function copysign(another change)"""
+"""This program is to determine the modular function (modf) of an argument"""
 import unittest
-# from wrapper_copysign import wrapper
+# from wrapper_modf import wrapper
 
-class TestCopysignMethod(unittest.TestCase):
-    """"This class has methods which check the validity of arguments for
-    the copysign wrapper"""
+class TestModfMethod(unittest.TestCase):
+    """"This class has methods which check the validity of an argument for
+    the modf wrapper"""
 
-    def test_wrapper_correct_arguments(self):
-        """This function checks if the arguments are of valid types"""
-        self.assertEqual(wrapper(1.79), 2)
+    def test_wrapper_correct_argument(self):
+        """This function checks if the argument are of a valid type"""
+        self.assertEqual(wrapper(1.79), 0.78)
 
-    def test_wrapper_incorrect_args(self):
-        """This function checks if the arguments are of valid types"""
+    def test_wrapper_incorrect_arg(self):
+        """This function checks if the arguments are of an invalid type"""
         with self.assertRaises(TypeError):
             wrapper(1)
 
-    def test_wrapper_no_args(self):
-        """This function checks if the arguments are of valid types"""
+    def test_wrapper_no_arg(self):
+        """This function checks if the arguments are of valid type"""
         with self.assertRaises(TypeError):
             wrapper()
 
---
+++ wrapper_modf.py
@@ -0,0 +1,5 @@
+# from math import modf
+
+def wrapper(argument1):
+    """Created an empty function for the first commit"""
+    pass
